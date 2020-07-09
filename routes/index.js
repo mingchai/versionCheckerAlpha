241fa6bf58b5c105f54e5d071e5c638eb1461f89
Ming Chai <ming.chai4@gmail.com>
2020-07-09 14:59:30 -0700

    create async fetcher function for request and redirect

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,43 @@ const express = require('express');
 const router = express.Router();
 const experiments = require('../experiments');
 const psFetcher = experiments.psFetcher;
+const {log, error} = console;
+const knex = require('../db/client');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Version Checker' });
+  knex.select("*").from("versionTracker").orderBy("createdAt", "DESC").then(versionInfo =>{
+    res.render("./index", {versionInfo, title: 'Version Checker'});
+})
 });
 
 router.post("/", (req, res) => {
-  console.log(req.body.urlInput);
-  let requestedURL = req.body.urlInput;
-  psFetcher(requestedURL);
-  res.redirect('/')
+  try {
+      async function fetcher(){
+        let requestedURL = req.body.urlInput;
+        let specifiedGrouping = req.body.grouping;
+        let fetchedVersion = await psFetcher(requestedURL);
+        log(requestedURL, specifiedGrouping)
+        const newEntry = {
+          url: requestedURL,
+          grouping: specifiedGrouping,
+          version: fetchedVersion
+        }
+      
+
+        log("redirecting...")
+        knex.insert(newEntry)
+          .into("versionTracker")
+          .returning('*')
+          .then( ()=>{
+            res.redirect("/");
+          })
+        }
+        fetcher();
+  } catch(err) {
+    error(err);
+  }
 })
 
 module.exports = router;
