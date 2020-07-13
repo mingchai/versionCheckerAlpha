102af2fd7e3a31307ee7190a53c350e828dff52f
Ming Chai <ming.chai4@gmail.com>
2020-07-13 07:49:24 -0700

    refactor: move variables & add conditions for index; move variables in experiments.js file

--- experiments.js
+++ experiments.js
@@ -1,60 +1,61 @@
 const axios = require("axios");
 const express = require("express");
-const app = express();
 
-let { log } = console;
-// let urlInput = process.argv[2];
+const { log } = console;
 let response;
 let data;
+const psApp = "PS";
+const ipsApp = "IPS" ;
+// Matches to just the version number
+// let versionNumberPattern = /\d(\.\d{1,2})+/gm;
 
-const psFetcher = async function fetcher(url) {
-  try {
-    response = await axios.get(`${url}/ps/dev/SystemStatus.action`);
-    if (response.status !== 200) {
-      log(`Page returned Status Code ${response.status}`);
-    }
-    data = response.data.split("\n");
+let versionNumberPatternWithHash = /\d{0,1}(\.\d{1,2})+\s-\s.{7}/gm;
+let snapshotVersion = /\d{0,1}(\.\d{1,2})+-.+\s-\s.{7}/gm;
 
-    let versionPatternWithHash = /\d{0,1}(\.\d{1,2})+-./gm;
-    let versionNumberPattern = /\d(\.\d{1,2})+/gm;
-    let versionNumberPattern1WithHash = /\d{0,1}(\.\d{1,2})+\s-\s.{7}/gm;
-    let snapshotVersion = /\d{0,1}(\.\d{1,2})+-.+\s-\s.{7}/gm;
+module.exports = {
+  psFetcher: async function psFetcher(url) {
+    try {
+      response = await axios.get(`${url}/ps/dev/SystemStatus.action`);
+      if (response.status !== 200) {
+        log(`Page returned Status Code ${response.status}`);
+      }
 
-    for (let elm of data) {
-      if (elm.includes("<td") && elm.match(snapshotVersion)) {
-        return elm.match(snapshotVersion).toString();
-      } else if (elm.includes("<td") && elm.match(versionPatternWithHash)) {
-        log(elm.match(versionNumberPattern).toString());
-        return elm.match(versionNumberPattern).toString();
-      } else if (
-        elm.includes("<td") &&
-        elm.match(versionNumberPattern1WithHash)
-      ) {
-        log(
-          `Version for ${url}: ${elm
-            .match(versionNumberPattern1WithHash)
-            .toString()}`
-        );
-        return elm.match(versionNumberPattern1WithHash).toString();
+      data = response.data.split("\n");
+
+      for (let elm of data) {
+        if (elm.includes("<td") && elm.match(snapshotVersion)) {
+          log(elm.match(snapshotVersion).toString());
+          return [elm.match(snapshotVersion).toString(), psApp];
+        } else if (
+          elm.includes("<td") &&
+          elm.match(versionNumberPatternWithHash)
+        ) {
+          log(
+            `Version for ${url}: ${elm
+              .match(versionNumberPatternWithHash)
+              .toString()}`
+          );
+          return [elm.match(versionNumberPatternWithHash).toString(), psApp];
+        }
+      }
+    } catch (error) {
+      log(error);
+      if (error.code == "ECONNREFUSED") {
+        return "Connection was refused - check the URL that was entered";
+      }
+      if (error.response.status !== 200) {
+        return `Page returned with status ${error.response.status}`;
       }
     }
-  } catch (error) {
-    log(error);
-    if (error.code == "ECONNREFUSED") {
-      return "Connection was refused - check the URL that was entered";
-    }
-    if (error.response.status !== 200) {
-      return `Page returned with status ${error.response.status}`;
-    }
-  }
+  },
+
+  ipsFetcher: async function ipsFetcher(url) {
+    response = await axios.get(`${url}/ips/pub/api/version`);
+    log(response.data);
+    let version = response.data;
+    return [version, ipsApp];
+  },
 };
-
-async function ipsFetcher(url) {
-  response = await axios.get(`${url}/ips/pub/api/version`);
-  log(response.data);
-}
-
-exports.psFetcher = psFetcher;
 
 // Matches 4.10.0 Version
 // /\d{0,1}(\.\d{1,2})+-.+\s-\s.+/gm
--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,68 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const experiments = require('../experiments');
+const experiments = require("../experiments");
 const psFetcher = experiments.psFetcher;
-const {log, error} = console;
-const knex = require('../db/client');
 
-router.get('/', function(req, res, next) {
-  knex.select("*").from("versionTracker").orderBy("grouping", "ASC").orderBy("createdAt", "DESC").then(versionInfo =>{
-    res.render("./index", {versionInfo, title: 'Version Checker'});
-})
+const { log, error } = console;
+const knex = require("../db/client");
+const { ipsFetcher } = require("../experiments");
+
+router.get("/", function (req, res, next) {
+  knex
+    .select("*")
+    .from("versionTracker")
+    .orderBy("grouping", "ASC")
+    .orderBy("createdAt", "DESC")
+    .then((versionInfo) => {
+      res.render("./index", { versionInfo, title: "Version Checker" });
+    });
 });
 
 router.post("/", (req, res) => {
-  try {
-      async function fetcher(){
-        let requestedURL = req.body.urlInput;
-        let specifiedGrouping = req.body.grouping;
-        let fetchedVersion = await psFetcher(requestedURL);
+  let application = req.body.apps;
+  let requestedURL = req.body.urlInput;
+  let specifiedGrouping = req.body.grouping;
+
+  if (application == "PS/IPS") {
+    try {
+      async function fetcher() {
+        const [psVersion, psAppName] = await psFetcher(requestedURL);
+        const [ipsVersion, ipsAppName] = await ipsFetcher(requestedURL);
+
         log(`URL submitted: ${requestedURL}, Grouping: ${specifiedGrouping}`);
-        
-        const newEntry = {
+
+        const newPsEntry = {
           url: requestedURL,
           grouping: specifiedGrouping,
-          version: fetchedVersion
-        }
+          version: psVersion,
+          appName: psAppName
+        };
 
-        log("redirecting...")
-        knex.insert(newEntry)
+        const newIpsEntry = {
+          url: requestedURL,
+          grouping: specifiedGrouping,
+          version: ipsVersion,
+          appName: ipsAppName
+        };
+
+        log("redirecting...");
+        knex
+          .insert([newPsEntry, newIpsEntry])
           .into("versionTracker")
-          .returning('*')
-          .then( ()=>{
+          .returning("*")
+          .then(() => {
             res.redirect("/");
-          })
-        }
-      
+          });
+      }
+
       fetcher();
-  } catch(err) {
-    error(err);
-    res.render("./index", {err})
+    } catch (err) {
+      error(err);
+      res.render("./index", { err });
+    }
+  } else if(application == "Tron/TXS"){
+    // TODO: add fetch for other applications
   }
-})
+});
 
 module.exports = router;
